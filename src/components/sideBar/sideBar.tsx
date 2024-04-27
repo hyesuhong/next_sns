3c1bf691a025b223c013f86593b31253a20304f3
Hyesu <honghs95@gmail.com>
2024-04-28 01:46:57 +0900

    fix: modify profile's pathname

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import Divider from '../common/divider';
-import { IcoHeart, IcoHome, IcoLogOut, IcoProfile } from '../icons';
+import { IcoHome, IcoLogOut, IcoProfile } from '../icons';
 import NavItem from './navItem';
 
 export default function SideBar() {
@@ -16,18 +16,12 @@ export default function SideBar() {
 					label='Home'
 					isActive={router.pathname === '/'}
 				/>
-				<NavItem
-					path='/users/1/liked'
-					icon={<IcoHeart />}
-					label='Liked'
-					isActive={router.pathname === '/user/[id]/liked'}
-				/>
 				<Divider className='my-2' />
 				<NavItem
 					path='/users/1'
 					icon={<IcoProfile />}
 					label='My Profile'
-					isActive={router.pathname === '/user/[id]'}
+					isActive={router.pathname === '/users/[id]'}
 				/>
 				<NavItem path='' icon={<IcoLogOut />} label='Log out' />
 			</ul>
