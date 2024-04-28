01cfaef20165135af07bce4979b480b4893cac48
Hyesu <honghs95@gmail.com>
2024-04-28 17:04:40 +0900

    feat: add user's profile page

--- src/components/common/profile.tsx
+++ src/components/common/profile.tsx
@@ -2,15 +2,18 @@ import { IcoProfile } from '../icons';
 
 type ProfileProps = {
 	imgUrl?: string;
+	className?: string;
 };
 
-export default function Profile({ imgUrl }: ProfileProps) {
+export default function Profile({ imgUrl, className }: ProfileProps) {
 	return (
-		<span className='flex h-12 w-12 overflow-hidden rounded-full border-2 border-sns-white'>
+		<span
+			className={`flex h-12 w-12 overflow-hidden rounded-full border-2 border-sns-white ${className}`}
+		>
 			{imgUrl ? (
 				<img src={imgUrl} alt='' className='h-full w-full object-cover' />
 			) : (
-				<IcoProfile className='m-auto h-10 w-10' />
+				<IcoProfile className='m-auto h-4/5 w-4/5' />
 			)}
 		</span>
 	);
--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,5 +1,26 @@
+import { Divider, Profile } from '@/components/common';
 import { GeneralLayout } from '@/components/layouts';
+import { Post } from '@/components/post';
 
 export default function Page() {
-	return <GeneralLayout>user profile</GeneralLayout>;
+	return (
+		<GeneralLayout>
+			<section className='mx-auto max-w-4xl overflow-hidden rounded border border-sns-grey-dark'>
+				<div className='h-60 bg-sns-grey-dark'>{/* cover image area */}</div>
+				<div className='relative p-4 pt-16'>
+					<Profile
+						className='absolute left-4 top-0 h-28 w-28 -translate-y-2/3 bg-sns-charcoal'
+						imgUrl='https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?size=338&ext=jpg&ga=GA1.1.553209589.1714176000&semt=ais'
+					/>
+					<h2 className='mb-2 text-3xl'>Username</h2>
+					<p>user description</p>
+				</div>
+			</section>
+			<section className='mx-auto mt-12 flex max-w-4xl flex-col gap-y-8'>
+				<Post isOwner />
+				<Divider lightness='DARK' />
+				<Post isOwner />
+			</section>
+		</GeneralLayout>
+	);
 }
